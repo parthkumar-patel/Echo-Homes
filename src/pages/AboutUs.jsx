1b7ceeb2a584979ef4ed8ff3be3beecdfc926700
Parth <parthp15@yahoo.com>
2024-03-24 10:26:31 -0700

    text added

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,7 +6,17 @@ const AboutUs = () => {
     <>
       <div className="aboutUs-container">
         <div className="book">
-          <p>Hello</p>
+          <p className="about-us-text">
+            Echo Homes, is designed to revolutionize the way interns relocate by
+            offering an affordable, secure, and community-driven home swapping
+            service. At the heart of HomeSwap is the commitment to facilitating
+            seamless transitions for interns who relocate for their career
+            advancement, allowing them to focus on their professional
+            development without the burden of high living costs. By minimizing
+            the financial and logistical barriers to relocation, HomeSwap
+            empowers interns to pursue opportunities, underpinning our core
+            values of accessibility, community, and innovation
+          </p>
           <div className="cover">
             <p>About Us</p>
           </div>
