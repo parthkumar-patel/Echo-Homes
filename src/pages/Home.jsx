a9066cc263aed2311e9c536f816e3ec14c9dda7a
bryan <bryanjiang168@gmail.com>
2024-03-23 21:57:36 -0700

    Added HomeTemplate, app and example

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
+import React from "react";
+import HomeTemplate from "./HomeTemplate";
+import  bedroomCabin from "../images/bedroom_cabin.jpg";
+import frontCabin from "../images/front_mountain_cabin.jpg";
+import kitchenCabin from "../images/kitchen_cabin.jpg";
+
+
 const Home = () => {
-  return <div className="text-center">Home Page</div>;
+  return (
+    <div className="text-center">
+      <HomeTemplate listing={listingExample}/>
+    </div>
+  ); 
+};
+
+
+const listingExample = {
+  title: "Mountainside Cabin",
+  photos: [
+    { url: frontCabin, description: "Front view of the cabin" },
+    { url: bedroomCabin, description: "Living room" },
+    { url: kitchenCabin, description: "Kitchen" },
+  ],
+  description: "A cozy cabin in the mountains perfect for your next getaway. Enjoy the peace and quiet of nature.",
+  location: "Mountain View, CO",
+  stayLength: "2 months", 
+  maxGuests: 4,
+  amenities: ["Wi-Fi", "Kitchen", "Parking",  "Heating"],
+  reviews: [
+    { comment: "I loved the views here, would recommend!", rating: 5 },
+    { comment: "Almost died to a bear but it had a nice view!", rating: 3 },
+    { comment: "Recommend bringing something to protect yourself", rating: 2 }
+  ],
 };
 
 export default Home;
+  
---
+++ src/pages/HomeTemplate.jsx
@@ -0,0 +1,47 @@
+import '../HomeListing.css';
+
+const HomeListing = ({ listing }) => {
+    return (
+        <div className='main'> 
+      <div className="listing-container">
+        <h2>{listing.title}</h2>
+        <div className="photos">
+          {listing.photos.map((photo, index) => (
+            <img key={index} src={photo.url} alt={photo.description} />
+          ))}
+        </div>
+        <div className="description">
+          <p>{listing.description}</p>
+        </div>
+        </div>
+        <div className='details_amenities'> 
+        <div className="details">
+          <h3>Details</h3>
+          <p>Location: {listing.location}</p>
+          <p>Preferred length: {listing.stayLength}</p>
+          <p>Maximum guests: {listing.maxGuests}</p>
+        </div>
+        <div className="amenities">
+          <h4>Amenities</h4>
+          <ul>
+            {listing.amenities.map((amenity, index) => (
+              <li key={index}>{amenity}</li>
+            ))}
+          </ul>
+        </div>
+        </div>
+        <div className="reviews">
+          <h3>Reviews</h3>
+          {listing.reviews.map((review, index) => (
+            <div key={index} className="review">
+              <p>{review.comment}</p>
+              <p>Rating: {review.rating}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default HomeListing;
+
